4a6803753108ba3a8cc95b623e13edb6b7680823
benjaminbills <benjaminoba234@gmail.com>
2021-12-15 18:25:56 +0300

    update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Home() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  let history = useNavigate();
+  useEffect(() => {
+    if (!userInfo) {
+      history("/login");
+    }
+    // dispatch({ type: BOOK_COLLECTED_RESET });
+  }, [userInfo, history]);
   return (
     <div className="padding container">
       <h2>Welcome</h2>
